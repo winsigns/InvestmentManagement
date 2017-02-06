c5c552ae8bedb397c0c6aee944d8fc62bd1de69b
colin <colingonglin@163.com>
2017-02-06 17:49:10 +0800

    Init

---
+++ FundService/src/main/java/com/winsigns/investment/FundServiceApplication.java
@@ -0,0 +1,12 @@
+package com.winsigns.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundServiceApplication.class, args);
+	}
+}
