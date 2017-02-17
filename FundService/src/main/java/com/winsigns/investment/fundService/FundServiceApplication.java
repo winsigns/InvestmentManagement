c966902b86c7eb86f09ce0bf94825ced17b771aa
unknown <>
2017-02-17 16:22:15 +0800

    删除不必要的注解

--- FundService/src/main/java/com/winsigns/investment/fundService/FundServiceApplication.java
+++ FundService/src/main/java/com/winsigns/investment/fundService/FundServiceApplication.java
@@ -1,18 +1,12 @@
 package com.winsigns.investment.fundService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 @SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableHypermediaSupport(type= {HypermediaType.HAL})
+@EnableHypermediaSupport(type = { HypermediaType.HAL })
 public class FundServiceApplication {
 
 	public static void main(String[] args) {
