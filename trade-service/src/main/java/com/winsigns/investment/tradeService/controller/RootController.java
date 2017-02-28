a399d2214e1d9edc164d755d653a8d7fc4dc4941
yiming.jin <yiming.jin@winsigns.com.cn>
2017-02-28 11:15:42 +0800

    交易服务功能增加

--- trade-service/src/main/java/com/winsigns/investment/tradeService/controller/RootController.java
+++ trade-service/src/main/java/com/winsigns/investment/tradeService/controller/RootController.java
@@ -1,6 +1,8 @@
 package com.winsigns.investment.tradeService.controller;
 
 import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -18,10 +20,12 @@ import com.winsigns.investment.tradeService.hal.HALResponse;
 
 @RestController
 public class RootController {
-    @GetMapping(path = "/", produces = { HAL_JSON_VALUE, APPLICATION_JSON_VALUE, APPLICATION_JSON_UTF8_VALUE })
-    public HttpEntity<HALResponse<String>> root() {
-        HALResponse<String> halResponse = new HALResponse<String>("");
-        // halResponse.add(linkTo(methodOn((FundController.class)).readFunds()).withRel("funds"));
-        return new ResponseEntity<>(halResponse, HttpStatus.OK);
-    }
+  @GetMapping(path = "/",
+      produces = {HAL_JSON_VALUE, APPLICATION_JSON_VALUE, APPLICATION_JSON_UTF8_VALUE})
+  public HttpEntity<HALResponse<String>> root() {
+    HALResponse<String> halResponse = new HALResponse<String>("");
+    halResponse.add(linkTo(methodOn(TradeController.class).getAvailableTradeServices())
+        .withRel("trade-services"));
+    return new ResponseEntity<>(halResponse, HttpStatus.OK);
+  }
 }
