321b881280eabf8e3d97f8770948ac660c580af7
jmx <mingxin.ji@winsigns.com.cn>
2017-03-01 15:45:47 +0800

    完善构建脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-cd edge-service && chmod +x ./gradlew && ./gradlew clean build && cd -
-cd discovery-service && chmod +x ./gradlew && ./gradlew clean build && cd -
-cd fund-service && chmod +x ./gradlew && ./gradlew clean build && cd -
-cd inventory-service && chmod +x ./gradlew && ./gradlew clean build && cd -
-cd invest-service && chmod +x ./gradlew && ./gradlew clean build && cd -
-cd trade-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+#兼容jenkins
+if [ -z ${WORKSPACE} ];then
+   WORKSPACE=.
+fi
+
+cd ${WORKSPACE}/edge-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+cd ${WORKSPACE}/discovery-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+cd ${WORKSPACE}/fund-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+cd ${WORKSPACE}/inventory-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+cd ${WORKSPACE}/invest-service && chmod +x ./gradlew && ./gradlew clean build && cd -
+cd ${WORKSPACE}/trade-service && chmod +x ./gradlew && ./gradlew clean build && cd -
