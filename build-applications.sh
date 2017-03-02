2a3215c0bfa7d91911054ffe2e46ad88df376cab
jmx <mingxin.ji@winsigns.com.cn>
2017-03-02 15:53:37 +0800

    make linux build happy

--- build-applications.sh
+++ build-applications.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd edge-service && ./gradlew clean build && cd -
-cd fund-service && ./gradlew clean build && cd -
-cd inventory-service && ./gradlew clean build && cd -
-cd invest-service && ./gradlew clean build && cd -
-cd trade-service && ./gradlew clean build && cd -
+cd edge-service && ./gradlew dev clean build && cd -
+cd fund-service && ./gradlew dev clean build && cd -
+cd inventory-service && ./gradlew dev clean build && cd -
+cd invest-service && ./gradlew dev clean build && cd -
+cd trade-service && ./gradlew dev clean build && cd -
