b021b8a026353892c725f119618dd3a6123c203c
jmx <mingxin.ji@winsigns.com.cn>
2017-03-04 11:14:20 +0800

    change the method of memory management

---
+++ inventory-service/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+
+# If not default limit_in_bytes in cgroup
+if [ "$limit_in_bytes" -ne "9223372036854771712" ]
+then
+    limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
+    heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
+    export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
+fi
+
+echo RESERVED_MEGABYTES=$RESERVED_MEGABYTES JAVA_OPTS=$JAVA_OPTS
+java $JAVA_OPTS -jar $1
