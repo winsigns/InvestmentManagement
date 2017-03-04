fabdd95cfe0aae0ed2365d735a602c5677c9760e
jmx <mingxin.ji@winsigns.com.cn>
2017-03-04 12:02:33 +0800

    set RESERVED_MEGABYTES default value to 64

--- discovery-service/start.sh
+++ discovery-service/start.sh
@@ -4,6 +4,9 @@ limit_in_bytes=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
 # If not default limit_in_bytes in cgroup
 if [ "$limit_in_bytes" -ne "9223372036854771712" ]
 then
+    if [ -z $RESERVED_MEGABYTES ]; then
+        RESERVED_MEGABYTES=64
+    fi
     limit_in_megabytes=$(expr $limit_in_bytes \/ 1048576)
     heap_size=$(expr $limit_in_megabytes - $RESERVED_MEGABYTES)
     export JAVA_OPTS="-Xmx${heap_size}m $JAVA_OPTS"
