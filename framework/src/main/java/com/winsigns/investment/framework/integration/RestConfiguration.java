6a0b165abf4cc89bd503728567234a07cad53d30
yiming.jin <yiming.jin@winsigns.com.cn>
2017-03-15 15:23:21 +0800

    修正

---
+++ framework/src/main/java/com/winsigns/investment/framework/integration/RestConfiguration.java
@@ -0,0 +1,15 @@
+package com.winsigns.investment.framework.integration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfiguration {
+
+  @Bean
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+}
