38faebef3a3f0171389f0816b762bf535e6eeed6
yiming.jin <yiming.jin@winsigns.com.cn>
2017-03-15 17:25:11 +0800

    优化取序列的方式

--- sequence-service/src/main/java/com/winsigns/investment/sequenceService/service/MeasureVersionService.java
+++ sequence-service/src/main/java/com/winsigns/investment/sequenceService/service/MeasureVersionService.java
@@ -20,18 +20,39 @@ public class MeasureVersionService {
   private static final String STR_MEARSURE_VERSION_FIX = "measure-version:";
 
   /*
+   * 每次从redis获取10000个序列
+   */
+  private static final Integer INTERVAL = 10000;
+
+  private Integer currentSequence = 0;
+
+  private Integer surplusSequence = 0;
+
+  private String nowDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
+
+  /*
    * 获取一个指标版本
    */
-  public String getMeasureVersion() {
-    String key = STR_MEARSURE_VERSION_FIX + getDate();
-    Integer nowVersion = redisTemplate.opsForValue().get(key);
-    if (nowVersion == null) {
-      nowVersion = 0;
-    } else {
-      nowVersion++;
+  public synchronized String getMeasureVersion() {
+    if (isSameDay()) {
+      String key = STR_MEARSURE_VERSION_FIX + nowDate;
+      if (!isEnough()) {
+        currentSequence = redisTemplate.opsForValue().get(key);
+        if (currentSequence == null)
+          currentSequence = 0;
+        incSequence();
+        redisTemplate.opsForValue().set(key, currentSequence + surplusSequence);
+      }
     }
-    redisTemplate.opsForValue().set(key, nowVersion);
-    return formatVersion(nowVersion);
+    // 如果不是同一天，则需要从0开始
+    else {
+      nowDate = getDate();
+      initSequence();
+      return getMeasureVersion();
+    }
+    String version = formatVersion(currentSequence++);
+    surplusSequence--;
+    return version;
   }
 
   private String getDate() {
@@ -41,6 +62,26 @@ public class MeasureVersionService {
 
   private String formatVersion(Integer nowVersion) {
     return String.format("%s%014d", getDate(), nowVersion);
+  }
+
+  private boolean isEnough() {
+    if (surplusSequence == 0) {
+      return false;
+    }
+    return true;
+  }
+
+  private void initSequence() {
+    currentSequence = 0;
+    surplusSequence = 0;
+  }
+
+  private void incSequence() {
+    surplusSequence = surplusSequence + INTERVAL;
+  }
+
+  private boolean isSameDay() {
+    return nowDate.equals(getDate());
   }
 
 }
