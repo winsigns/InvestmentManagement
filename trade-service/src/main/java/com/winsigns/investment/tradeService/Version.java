6edd87dd431d8dd3ccda150aa3a60b0d0d3f3366
jmx <mingxin.ji@winsigns.com.cn>
2017-03-17 09:37:08 +0800

    增加显示版本号的日志。清理代码

---
+++ trade-service/src/main/java/com/winsigns/investment/tradeService/Version.java
@@ -0,0 +1,21 @@
+package com.winsigns.investment.tradeService;
+
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jmx on 3/10/17.
+ */
+@Component
+public class Version {
+  @Value("${app.version}")
+  private String version;
+
+  @PostConstruct
+  public void showVersion(){
+    Logger log = Logger.getLogger(Version.class.getName());
+    log.info(String.format("######VERSION######: %s", version));
+  }
+}
