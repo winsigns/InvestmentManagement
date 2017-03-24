d2e154a0eab95967a5186cb9fe8e685e1768fbd3
jmx <mingxin.ji@winsigns.com.cn>
2017-03-24 10:52:49 +0800

    删除不必要的调试信息

--- continuous-deploy.sh
+++ continuous-deploy.sh
@@ -1,18 +1,10 @@
 CURR_TAG=${COMMIT_BUILD_TYPE}${COMMIT_BUILD_NUMBER}
 
-echo "BEFORE:"
-docker images
-
 for name in discovery-service fund-service trade-service invest-service inventory-service web-gateway sequence-service
 do
     docker rmi docker-registry:5000/${name}:latest
     docker tag docker-registry:5000/${name}:${CURR_TAG} docker-registry:5000/${name}:latest
 done
-
-echo "AFTER:"
-docker images
-
-echo "cd ${COMMIT_WORKSPACE}"
 
 cd ${COMMIT_WORKSPACE}
 docker-compose -f infrastructure-cd.yml down
@@ -22,7 +14,7 @@ HOST_IP=$(ip route|awk '/default/ { print $3 }')
 
 # wait for infrastructures startup complete. 
 bash ./wait-for-it.sh -t 0 ${HOST_IP}:3306
-sleep 20s
+sleep 30s
 
 ./createdb.sh ${HOST_IP}
 ./migratedb.sh ${HOST_IP}
