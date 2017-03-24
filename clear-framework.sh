11ad548ba6a60a966bf6af67fa04eab1f80fc077
jmx <mingxin.ji@winsigns.com.cn>
2017-03-24 14:33:17 +0800

    add clear-framework.sh to make sure the newest framework can be used

---
+++ clear-framework.sh
@@ -0,0 +1,3 @@
+rm -rf $HOME/.gradle/caches/modules-2/files-2.1/com.winsigns/investment-framework/*
+rm -rf $HOME/.gradle/caches/modules-2/metadata-2.16/descriptors/com.winsigns/investment-framework/*
+
